85fe2167d738340f6c65c70166159d92c7d30df2
talraj bhatia <talrajsingh13@gmail.com>
2025-08-12 14:16:53 +0530

    first commit

---
+++ src/context/AppContext.jsx
@@ -0,0 +1,69 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { dummyProducts } from "../assets/assets";
+import toast from "react-hot-toast";
+
+export const AppContext = createContext();
+
+export const AppContextProvider = ({ children }) => {
+    const currency = import.meta.VITE_CURRENCY;
+  const navigate = useNavigate();
+  const [user, setUser] = useState(true);
+  const [isSeller, setIsSeller] = useState(false);
+  const [showUserLogin, setShowUserLogin] = useState(false);
+  const [products,setProducts] = useState([])
+  const [cartItems,setCartItems] = useState({})
+
+// fetch
+  const fetchProducts = async ()=>{
+    setProducts(dummyProducts)
+  }
+
+//  cart
+const addToCart = (itemId) =>{
+    let cartData = structuredClone(cartItems);
+
+    if(cartData[itemId]){
+        cartData[itemId] += 1;
+    }else{
+        cartData[itemId] = 1;
+    }
+    setCartItems(cartData);
+    toast.success("Added to the cart");
+} 
+
+// Update Cart Item Quantity
+const updateCartItem = (itemId,quantity) =>{
+    let cartData = structuredClone(cartItems);
+    cartData[itemId] = quantity;
+    setCartItems(cartData)
+    toast.success("Cart Updated")
+}
+
+// Remove from cart
+const removeFromCart = (itemId)=>{
+    let cartData = structuredClone(cartItems);
+    if(cartData[itemId]){
+        cartData[itemId] -= 1;
+        if(cartData[itemId] === 0){
+            delete cartData[itemId];
+        }
+    }
+    setCartItems(cartData);
+    toast.success("Item removed Successfully") 
+}
+
+  useEffect(()=>{
+    fetchProducts()
+  },[])
+
+  const value = { navigate, user, setUser, isSeller, setIsSeller ,showUserLogin,setShowUserLogin,products,currency,addToCart,updateCartItem,removeFromCart,cartItems};
+
+  return (
+    <AppContext.Provider value={value}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useAppContext = () => useContext(AppContext);
